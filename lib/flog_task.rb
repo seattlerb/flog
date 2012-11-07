a9a63df69b910f4b5fa386e02cdeb05a8fb103b9
Andrew Selder <andrew.selder@wizecommerce.com>
2012-11-07 15:56:09 -0800

    Add max_score methods, make FlogTask work.
    Add the max_score and max_method to Flog.
    max_score returns the score of the highest scoring method
    max_method returns and an array of [method_name, score] for the highest
    scoring method.
    
    Adjusted FlogTask to be able to fail either on total flog score or high
    scoring method threshholds.
    
    Made FlogTask actually work (option[:parser] was never getting set)

--- lib/flog_task.rb
+++ lib/flog_task.rb
@@ -3,12 +3,16 @@ class FlogTask < Rake::TaskLib
   attr_accessor :dirs
   attr_accessor :threshold
   attr_accessor :verbose
+  attr_accessor :fail_method
+  attr_accessor :parser
 
   def initialize name = :flog, threshold = 200, dirs = nil
     @name      = name
     @dirs      = dirs || %w(app bin lib spec test)
     @threshold = threshold
+    @fail_method = :total
     @verbose   = Rake.application.options.trace
+    @parser    = :RubyParser
 
     yield self if block_given?
 
@@ -22,12 +26,31 @@ class FlogTask < Rake::TaskLib
     task name do
       require "flog"
       flog = Flog.new
+      flog.options[:parser] = case parser
+      when :RubyParser
+        RubyParser
+      when :Ruby18Parser
+        Ruby18Parser
+      when :Ruby19Parser
+        Ruby19Parser
+      else
+        raise "Unknown parser"
+      end
       flog.flog(*dirs)
+       max_method, max_score = flog.max_method #need to grab these values before report resets them.
       flog.report if verbose
 
-      raise "Flog total too high! #{flog.total} > #{threshold}" if
-        flog.total > threshold
+      case fail_method
+      when :total
+        raise "Flog total too high! #{flog.total} > #{threshold}" if
+          flog.total > threshold
+      when :max_method
+        raise "Flog score for method #{max_method} too high! #{max_score} > #{threshold}" if max_score > threshold
+      else
+        raise "Unknow value of fail_method"
+      end
     end
+    
     self
   end
 end
